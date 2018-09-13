67200b73a8de25bfb2c02e67cbbdb0ecf3ca55c9
dreamerfable <dreamerfable@gmail.com>
2018-09-13 20:56:29 +0800

    remove spring security,I can't hold it right now. T_T

--- readinglist/src/main/java/com/dreamerfable/demo/readinglist/Reader.java
+++ readinglist/src/main/java/com/dreamerfable/demo/readinglist/Reader.java
@@ -1,19 +1,11 @@
 package com.dreamerfable.demo.readinglist;
 
-import java.util.Arrays;
-import java.util.Collection;
-
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
 @Entity
-public class Reader implements UserDetails {
+public class Reader {
 
-	private static final long serialVersionUID = 1L;
 	@Id
 	private String username;
 	private String fullname;
@@ -59,31 +51,6 @@ public class Reader implements UserDetails {
 	 */
 	public void setPassword(String password) {
 		this.password = password;
-	}
-
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return Arrays.asList(new SimpleGrantedAuthority("READER"));
-	}
-
-	@Override
-	public boolean isAccountNonExpired() {
-		return true;
-	}
-
-	@Override
-	public boolean isAccountNonLocked() {
-		return true;
-	}
-
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return true;
-	}
-
-	@Override
-	public boolean isEnabled() {
-		return true;
 	}
 
 }
